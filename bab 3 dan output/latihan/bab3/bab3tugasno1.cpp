ac5ca0b869c65388366ae60673cc41a9f9684570
Rifki Agus Septiyanto <43834360+RifkiAgus776@users.noreply.github.com>
2018-10-04 23:23:20 +0700

    Add files via upload

---
+++ bab 3 dan output/latihan/bab3/bab3tugasno1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+	{
+	
+	int angka;
+	cout<<"Ganjil atau genap"<<endl;
+	cout<<"masukan angka :"<<endl;
+	cin>>angka;
+	 
+	 if( angka % 2 ==0){
+	 
+	 cout<<"genap";	
+	 		
+	 }else {
+	 	cout<<"ganjil";
+	 }
+	
+	}
---
+++ bab 3 dan output/latihan/bab3/bab3tugasno2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+	{
+	
+	double c;
+	cout<<"Masukan suhu :";
+	cin>>c;
+	
+	if( c >20){
+		
+		cout<<"Panas";
+		
+	}else{
+		cout<<"Dingin";
+	}	
+}
